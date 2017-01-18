1b31ba1fc2ad754032b7a485bbf26e8c940a7306
Mattias Josephsson <josephson199@gmail.com>
2017-01-18 21:20:39 +0100

    setting up seed data
    setting up seed data

---
+++ src/PizzeriaWebAppASPNET_MVC_CORE/Infrastructure/IdentitySeedData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PizzeriaWebAppASPNET_MVC_CORE.Infrastructure
+{
+    public static class IdentitySeedData
+    {
+        private const string AdminUser = "Admin";
+        private const string AdminPassword = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+
+            UserManager<IdentityUser> userManager = app.ApplicationServices
+                .GetRequiredService<UserManager<IdentityUser>>();
+
+            IdentityUser user = await userManager.FindByNameAsync(AdminUser);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                await userManager.CreateAsync(user, AdminPassword);
+            }
+
+        }
+
+    }
+}
